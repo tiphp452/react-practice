f5e047065e08a8990893277465e21b7d14bef231
tiphp452 <tiphp452@gmail.com>
2020-11-24 19:58:30 +0900

    202011241958

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Title from "./Header/Title"
 
 export default class Header extends React.Component {
+  handleChange(e) {
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
   render() {
     return (
-      <header>header</header>
+      <header>
+        <Title title={this.props.title} />
+        <input value={this.props.title} onChange={ this.handleChange.bind(this)} />
+      </header>
     );
   }
 }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,11 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { title: "Welcome!!" };
+  }
+  changeTitle(title) {
+    this.setState({ title });
+  }
   render() {
-    let components = [<Header />,<Footer />]
     return (
       <div>
-        {components}
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+        <Footer />
       </div>
     );
   }
